09b65c2af33e8f710947262e61928cfa3857c92c
Boris Rorsvort <boris@rorsvort.com>
2020-10-03 23:59:07 +0200

    wip sidebar

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
 import "./App.css";
 import Joyride from "react-joyride";
 
-import { Hidden, Typography } from "@material-ui/core";
-import Filters from "@material-ui/icons/Search";
-import startCase from "lodash/startCase";
-import React from "react";
+import React, { useEffect } from "react";
 import AppBar from "@material-ui/core/AppBar";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 import Sets from "./components/Sets";
-import Toolbar from "@material-ui/core/Toolbar";
+
 import Tunebook from "./components/Tunebook";
 import { connect } from "react-redux";
 import { layoutStyles } from "./styles/layout";
 import { logout } from "./actions/session";
-import smallLogoUrl from "./images/logo-small.svg";
+
 import { toggleDrawer, toggleFilters } from "./actions/ui";
 import { withStyles } from "@material-ui/core/styles";
 import { withRouter } from "react-router-dom";
 import NavDrawer from "./components/NavDrawer";
 import { steps, stepsStyles } from "./helpers/welcomeTour";
+import { useCookies } from "react-cookie";
+import AppBarContent from "./components/AppBarContent";
 
 function App(props) {
+  const [cookies, setCookie] = useCookies();
+
   const handleDrawerToggle = () => {
     props.toggleDrawer();
   };
   const handleToggleFilters = () => {
     props.toggleFilters();
   };
+
+  useEffect(() => {
+    if (!cookies.hadConfigTour) {
+      setCookie("hadConfigTour", true);
+    }
+  }, [cookies, setCookie]);
 
   const {
     classes,
@@ -42,39 +47,16 @@ function App(props) {
         steps={steps}
         continuous={true}
         showProgress={true}
+        run={!cookies.hadConfigTour}
         disableScrolling={true}
         styles={stepsStyles}
       />
       <AppBar className={classes.appBar}>
-        <Toolbar>
-          <IconButton
-            id="navToggle"
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerToggle}
-            className={classes.navIconHide}
-          >
-            <MenuIcon />
-          </IconButton>
-          <div className={classes.flex}>
-            <Hidden smDown>
-              <img src={smallLogoUrl} alt="Foinn" className={classes.logo} />
-            </Hidden>
-            <Typography className={classes.title} variant="h6">
-              — {startCase(folder)}
-            </Typography>
-          </div>
-          <Hidden mdUp>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              onClick={handleToggleFilters}
-              className={classes.navIconHide}
-            >
-              <Filters />
-            </IconButton>
-          </Hidden>
-        </Toolbar>
+        <AppBarContent
+          folder={folder}
+          onToggleFilters={handleToggleFilters}
+          onDrawerToggle={handleDrawerToggle}
+        />
       </AppBar>
       <NavDrawer onClose={handleDrawerToggle} open={showDrawer} />
       <div className={classes.content}>
